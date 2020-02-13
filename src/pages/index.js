e50d1fe77dd29b990a188a7da5d6513506251ede
eduardo-exists <e_escoto@ucsb.edu>
2020-02-12 20:36:41 -0800

    was working already lel

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => (
           Projects
         </Link>
         &nbsp;
-        <Link to="/static/eduardo_escoto_resume.pdf" secondary button>
+        <Link to="/eduardo_escoto_resume.pdf" secondary button>
           Resume
         </Link>
       </p>
