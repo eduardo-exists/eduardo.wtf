4f03ff04549590a07cfbf1c9c03b028f6c631b55
Eduardo Escoto <me@eduardo.wtf>
2020-07-01 00:02:47 -0700

    updating resume

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,14 +2,15 @@ module.exports = {
   siteMetadata: {
     title: `Eduardo Exists`,
     description: `A centralized hub about ed.`,
-    author: `@gatsbyjs`,
+    author: `@eduardo-exists`,
+    url: 'https://eduardo.wtf'
   },
   plugins: [
     'gatsby-plugin-root-import',
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-mdx`,
     `gatsby-plugin-react-helmet`,
-    // `gatsby-remark-reading-time`,
+    `gatsby-remark-reading-time`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -22,8 +23,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `eduardo-exists`,
+        short_name: `eduardo-existis`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
